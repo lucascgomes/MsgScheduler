91bdda27be2cd982a9fe60157d6711873bab4629
Lucas Gomes <lucgms@gmail.com>
2015-01-25 14:41:44 -0200

    Saving scheduled time in SharedPrefs

--- MessagesScheduler/src/com/example/messagesscheduler/MainActivity.java
+++ MessagesScheduler/src/com/example/messagesscheduler/MainActivity.java
@@ -1,19 +1,20 @@
 package com.example.messagesscheduler;
 
 import java.util.Calendar;
-import java.util.Scanner;
 
 import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity implements TimePickerFragment.Listener{
 	
@@ -24,6 +25,22 @@ public class MainActivity extends Activity implements TimePickerFragment.Listene
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		SharedPreferences clock_settings = getSharedPreferences("CLOCK_SETTINGS", Context.MODE_PRIVATE);
+		int scheduled_hour = clock_settings.getInt("hour", -1);
+		int scheduled_minute = clock_settings.getInt("minute", -1);
+		
+		if (scheduled_hour == -1){
+			setTextClockTime(14, 00);
+			SharedPreferences.Editor editor = clock_settings.edit();
+			editor.putInt("hour", 14);
+			editor.putInt("minute", 00);
+			editor.commit();
+			Toast.makeText(getApplicationContext(), "Welcome for the first time!", Toast.LENGTH_LONG).show();
+		}
+		else{
+			setTextClockTime(scheduled_hour, scheduled_minute);
+		}
 	}
 
 	@Override
@@ -57,8 +74,19 @@ public class MainActivity extends Activity implements TimePickerFragment.Listene
 		timePickerDialog.setListener(this);
 	}
 	
-	//Callback is being called twice?
-	public void setTime(int hourOfDay, int minute) {
+	private void setAlarm(int hourOfDay, int minute){
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTimeInMillis(System.currentTimeMillis());
+		calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+		calendar.set(Calendar.MINUTE, minute);
+		
+		alarmMgr = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+		Intent intent = new Intent(this, AlarmReceiver.class);
+		alarmIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
+		alarmMgr.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, alarmIntent);	
+	}
+	
+	private void setTextClockTime(int hourOfDay, int minute){
 		TextView scheduledTime = (TextView) findViewById(R.id.textView2);
 		String hourStr = "" + hourOfDay;
 		String minuteStr = "" + minute;
@@ -69,16 +97,18 @@ public class MainActivity extends Activity implements TimePickerFragment.Listene
 			minuteStr = "0" + minuteStr;
 		}
 		scheduledTime.setText(hourStr + ":" + minuteStr);
+	}
+	
+	//Callback is being called twice?
+	public void setTime(int hourOfDay, int minute) {
+		setTextClockTime(hourOfDay, minute);
+		setAlarm(hourOfDay, minute);
 		
-		Calendar calendar = Calendar.getInstance();
-		calendar.setTimeInMillis(System.currentTimeMillis());
-		calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
-		calendar.set(Calendar.MINUTE, minute);
-		
-		alarmMgr = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-		Intent intent = new Intent(this, AlarmReceiver.class);
-		alarmIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
-		alarmMgr.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, alarmIntent);
+		SharedPreferences clock_settings = getSharedPreferences("CLOCK_SETTINGS", Context.MODE_PRIVATE);
+		SharedPreferences.Editor editor = clock_settings.edit();
+		editor.putInt("hour", hourOfDay);
+		editor.putInt("minute", minute);
+		editor.commit();
 	}
 	
 }

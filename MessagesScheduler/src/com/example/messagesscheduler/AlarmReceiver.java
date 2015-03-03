b4161db211d95fecc4151a75d9158f88fe82b87b
Lucas Gomes <lucgms@gmail.com>
2015-03-03 14:55:05 -0300

    Verifying if the message is being sent

--- MessagesScheduler/src/com/example/messagesscheduler/AlarmReceiver.java
+++ MessagesScheduler/src/com/example/messagesscheduler/AlarmReceiver.java
@@ -3,6 +3,7 @@ package com.example.messagesscheduler;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -12,9 +13,9 @@ import android.util.Log;
 import android.widget.EditText;
 
 public class AlarmReceiver extends BroadcastReceiver {
-	
+
 	private SmsManager smsManager;
-	
+
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		// Read line from the text file
@@ -22,32 +23,42 @@ public class AlarmReceiver extends BroadcastReceiver {
 		String message;
 		String love_letter_emoticon = "\uD83D\uDC8C";
 		String phone_number = "+55 35 9254-6711";
+
+//		SharedPreferences file_prefs = context.getSharedPreferences("FILE_PREFS", Context.MODE_PRIVATE);
+//		file_line = file_prefs.getInt("file_line", 0);
+//
+//		Scanner s = new Scanner(context.getResources().openRawResource(R.raw.messages));
+//		try {
+//			for (int i = 0; i < file_line; i++) {
+//				s.nextLine();
+//			}
+//			message = s.nextLine();
+//			Log.d("MESSAGE-->", message);
+//		} finally {
+//			s.close();
+//		}
+//
+//		file_line++;
+//		SharedPreferences.Editor editor = file_prefs.edit();
+//		editor.putInt("file_line", file_line);
+//		editor.commit();
 		
-		SharedPreferences file_prefs = context.getSharedPreferences("FILE_PREFS", Context.MODE_PRIVATE);
-		file_line = file_prefs.getInt("file_line", 0);
-		
-		Scanner s = new Scanner(context.getResources().openRawResource(R.raw.messages));
-		try {
-			for (int i=0; i<file_line; i++){
-				s.nextLine();
-			}
-		    message = s.nextLine();
-	        Log.d("MESSAGE-->", message);
-		} finally {
-		    s.close();
-		}
-		
-		file_line++;
-		SharedPreferences.Editor editor = file_prefs.edit();
-		editor.putInt("file_line", file_line);
-		editor.commit();
-		
+		/************************* TEST **********************************/
+		message = "teste";
+		/*****************************************************************/
+
 		EditText myMessage = new EditText(context);
-		myMessage.getText().append(love_letter_emoticon + " " + message );
-		
+		myMessage.getText().append(love_letter_emoticon + " " + message);
+
+		/* SMS */
 		smsManager = SmsManager.getDefault();
 		ArrayList<String> messageParts = smsManager.divideMessage(myMessage.getText().toString());
-		smsManager.sendMultipartTextMessage(phone_number, null, messageParts, null, null);
+		ArrayList<PendingIntent> sentPendingIntents = new ArrayList<PendingIntent>();
+		PendingIntent sentPI = PendingIntent.getBroadcast(context, 0, new Intent(context, SmsSentReceiver.class), 0);
+		for (int i = 0; i < messageParts.size(); i++) {
+			sentPendingIntents.add(i, sentPI);
+		}
+		smsManager.sendMultipartTextMessage(phone_number, null, messageParts, sentPendingIntents, null);
 	}
 
 }
---
+++ MessagesScheduler/src/com/example/messagesscheduler/SmsSentReceiver.java
@@ -0,0 +1,56 @@
+package com.example.messagesscheduler;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class SmsSentReceiver extends BroadcastReceiver {
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		String contentTitle;
+		String contentText;
+		
+		if (getResultCode() == Activity.RESULT_OK){
+			contentTitle = "Message sent";
+			contentText = "Click to see the message";
+		}
+		else{
+			contentTitle = "Message not sent";
+			contentText = "Try again later";
+		}
+		
+		Notification.Builder mBuilder =
+		        new Notification.Builder(context)
+		        .setSmallIcon(R.drawable.ic_love_letter)
+		        .setContentTitle(contentTitle)
+		        .setContentText(contentText);
+		// Creates an explicit intent for an Activity in your app
+		Intent resultIntent = new Intent(context, MainActivity.class);
+
+		// The stack builder object will contain an artificial back stack for the
+		// started Activity.
+		// This ensures that navigating backward from the Activity leads out of
+		// your application to the Home screen.
+		TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+		// Adds the back stack for the Intent (but not the Intent itself)
+		stackBuilder.addParentStack(MainActivity.class);
+		// Adds the Intent that starts the Activity to the top of the stack
+		stackBuilder.addNextIntent(resultIntent);
+		PendingIntent resultPendingIntent =
+		        stackBuilder.getPendingIntent(
+		            0,
+		            PendingIntent.FLAG_UPDATE_CURRENT
+		        );
+		mBuilder.setContentIntent(resultPendingIntent);
+		NotificationManager mNotificationManager =
+		    (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+		// mId allows you to update the notification later on.
+		mNotificationManager.notify(1, mBuilder.build());
+	}
+}
